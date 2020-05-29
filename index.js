ffa3e03341aebf317ec975d7db38e5337828becc
Miguel Duque <mduquem1@gmail.com>
2020-05-28 20:01:18 -0500

    Add mddleware

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const jwt = require("jsonwebtoken");
+const server = express();
+
+server.use(bodyParser.json());
+
 const info = {
-    name: "Juan Felipe"
-}
-const signature = "my_secret_password";
-const token = jwt.sign(info, signature);
-console.log(token); // Outputs encryption algorithm: Specific, encrypted word that can be decrypted using a key
+   name: 'Juan Felipe',
+};
+
+const users = [
+   {
+      user: 'Test',
+      password: 'test',
+   },
+   {
+      user: 'Prueba',
+      password: 'prueba',
+   },
+   {
+      user: 'Validar',
+      password: 'validar',
+   },
+];
+
+const signature = 'my_secret_password';
+// console.log(token);
+// Outputs encryption algorithm: Specific, encrypted word that can be decrypted using a key
 
 // Verify signature
-const decoded = jwt.verify(token, signature);
-console.log(decoded);
+// const decoded = jwt.verify(token, signature);
+// console.log(decoded);
 
+const jwtMiddleware = (req, res, next) => {
+   const { user, password } = req.body;
+   const validated = validateUserPassword(user, password);
+   if (validated) {
+      next();
+   } else {
+      res.status(401).json({ error: 'Invalid credentials' });
+   }
+};
+
+const validateUserPassword = (user, password) => {
+   let valid = false;
+   users.map((existingUser) => {
+      if (existingUser.user == user && existingUser.password == password) {
+         valid = true;
+      }
+   });
+   return valid;
+};
+
+server.post('/login', [jwtMiddleware], (req, res) => {
+   const { user, password } = req.body;
+
+   res.json({ token: jwt.sign(user, signature) });
+});
+
+server.listen(3000, () => {
+   console.log('Server running...');
+});
