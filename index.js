8ed1e0ea8e154f1cf079a3bbc1757f6a40006d59
alpaar <alejandro.palacios88@gmail.com>
2020-05-28 21:34:36 -0500

    update

--- index.js
+++ index.js
@@ -53,11 +53,31 @@ const validateUserPassword = (user, password) => {
    return valid;
 };
 
+
+const userAutenticate = (req, res, next) => {
+   try {
+      const token = req.headers.authorization.split(' ')[1];
+      const verificarToken = jwt.verify(token, signature);
+      if (verificarToken){
+         req.usuario = verificarToken;
+         return next();
+      }
+   }catch(err){
+      res.json({ error: 'Error al validar usuario'});
+   }
+};
+
+
 server.post('/login', [jwtMiddleware], (req, res) => {
    const { user, password } = req.body;
 
    res.json({ token: jwt.sign(user, signature) });
 });
+
+server.get('/secure', userAutenticate, (req, res) => {
+   res.send(`Esta es una página autenticada. Hola ${req.usuario}`);
+})
+
 
 server.listen(3000, () => {
    console.log('Server running...');
